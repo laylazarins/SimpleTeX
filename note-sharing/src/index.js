7931004f16986f40c8686be1a00a5cbf3f8b459a
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2023-11-19 10:42:20 -0800

    todo

--- note-sharing/src/index.js
+++ note-sharing/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 import './index.css';
 import App from './App';
@@ -19,14 +19,6 @@ const rootElement = document.getElementById("root");
       </BrowserRouter>,
       rootElement
     );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-  
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
