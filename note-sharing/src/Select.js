42d6c683a9546f7d03e2637b7c9627101cdb170d
elainechen <zchen97@students.cs.ubc.ca>
2023-11-19 09:58:00 -0800

    message

---
+++ note-sharing/src/Select.js
@@ -0,0 +1,12 @@
+import './App.css';
+//import Landing from './components/landing-page.js';
+
+function Select() {
+    return (
+      <div className="SelectScreen">
+      <p1>hello there </p1>
+      </div>
+    );
+  }
+  
+  export default Select;
