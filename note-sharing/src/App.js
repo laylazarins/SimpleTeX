8cbd375f0fd6585209bcca11e3908184f4735e36
v1231029 <vacuumou@gmail.com>
2023-11-19 09:25:19 -0800

    React Testing

--- note-sharing/src/App.js
+++ note-sharing/src/App.js
@@ -1,1 +1,13 @@
 import './App.css';
+import Thing from './components/thing.js';
+
+function App() {
+  return (
+    <div className="thing">
+    <Thing />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ note-sharing/src/components/thing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Thing() {
+    return (
+        <div>
+            <h1>Thing</h1>
+        </div>
+    )
+}
