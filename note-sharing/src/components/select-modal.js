c56071d774d96914f076b2149262ecd7e99351b4
layla <lzarins@student.ubc.ca>
2023-11-19 15:30:02 -0800

    :3

--- note-sharing/src/components/select-modal.js
+++ note-sharing/src/components/select-modal.js
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
 import './select-modal.css'; // Import your Modal CSS
 
-const Modal = ({ show, children, onClose, submit}) => {
+const Modal = ({ show, onClose, submit }) => {
+  const [title, setTitle] = useState(''); // State to store the input value
+
+  const handleInputChange = (e) => {
+    setTitle(e.target.value); // Update the state on input change
+  };
+
+  const handleSubmit = () => {
+    submit(title); // Call the submit function passed as a prop with the input value
+    setTitle(''); // Optionally clear the input field after submit
+    onClose(); // Close the modal
+  };
+
   if (!show) {
     return null;
   }
@@ -8,9 +21,13 @@ const Modal = ({ show, children, onClose, submit}) => {
   return (
     <div className="modal-backdrop">
       <div className="modal-content">
-        {children}
-        <input type="text" placeholder="title" />
-        <button onClick={submit}>Create</button>
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSubmit}>Create</button>
         <button onClick={onClose}>Cancel</button>
       </div>
     </div>
